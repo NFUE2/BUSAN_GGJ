d9f87cbc147d8a6f3c12d02457952312d74fbfa7
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-01-28 05:17:36 +0900

    ui수정

--- BUSAN_GGJ/Assets/Scripts/OptionBox.cs
+++ BUSAN_GGJ/Assets/Scripts/OptionBox.cs
@@ -6,13 +6,23 @@ public class OptionBox : MonoBehaviour
 {
     [SerializeField]
     private AudioMixer mixer;
+    [SerializeField] private Slider[] slider;
+
+    private void Start()
+    {
+        float value;
+        for (int i = 0; i < slider.Length; i++)
+        {
+            mixer.GetFloat(slider[i].name,out value);
+            slider[i].value = value;
+        }
+    }
 
     public void Set_Volume(Slider slider)
     {
         string type = slider.name;
         mixer.SetFloat(type, slider.value);
 
-        Debug.Log($"{type} {slider.value}");
 
         if(slider.value == -20)
             mixer.SetFloat(type, -80f);
@@ -20,6 +30,8 @@ public class OptionBox : MonoBehaviour
             mixer.SetFloat(type, slider.value);
         float value;
         mixer.GetFloat(type, out value);
-        Debug.Log(value);
+
+        if (type == "BGM") GameManager.Instance.bgm_vol = value;
+        else GameManager.Instance.eff_vol = value;
     }
 }
